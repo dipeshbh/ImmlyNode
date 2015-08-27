e566faf6db20b25182b6d9d4e55297e492e6445a
dipeshbh <dipeshbh@gmail.com>
2015-08-27 01:00:21 -0700

    v 0.0.1

---
+++ routes/xmlparsertest.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+
+function retrieveFeedSource() {
+        console.log("inside retrieveFeedSource function")
+        var FeedSource = Parse.Object.extend("FeedSource");
+        console.log("FeedSource");
+        var query = new Parse.Query(FeedSource);
+        query.find({
+            success: function(results) {
+                console.log("results"+results.length);
+                for(var i = 0; i < results.length; i++) {
+                  console.log("results");
+                  var source = results[i];
+                  var sourceName = source.get('sourceName');
+                  console.log("sourcename" + sourceName)
+                }
+            },
+            error: function(error) {
+              alert("Error: " + error.code + " " + error.message);
+            }
+        });   
+}
+
+/* GET home page. */
+router.get('/parse', function(req, res, next) {
+    retrieveFeedSource();
+    res.render('hello', {});
+});
+
+module.exports = router;
+
+
