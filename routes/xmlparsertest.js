b5199c2d02096bfd25bbb23f3015f48da6671249
dipeshbh <i836816@MacBook-Pro.local>
2015-08-28 19:00:03 -0700

    added xmlparsing package

--- routes/xmlparsertest.js
+++ routes/xmlparsertest.js
@@ -1,33 +1,62 @@
 var express = require('express');
 var router = express.Router();
+var feed = require('feed-read-master');
 
 
-function retrieveFeedSource() {
-        console.log("inside retrieveFeedSource function")
+
+function retrieveFeedSource(res) {
+        //console.log("inside retrieveFeedSource function")
         var FeedSource = Parse.Object.extend("FeedSource");
-        console.log("FeedSource");
+        //console.log("FeedSource");
         var query = new Parse.Query(FeedSource);
+        var returnResults = [{}];
+
         query.find({
             success: function(results) {
-                console.log("results"+results.length);
+                
+                
                 for(var i = 0; i < results.length; i++) {
-                  console.log("results");
-                  var source = results[i];
-                  var sourceName = source.get('sourceName');
-                  console.log("sourcename" + sourceName)
+                  //console.log("results");
+                  //returnResults[i] = {};
+                  var sourceName = results[i].get("sourceName");
+                  var fileURL = results[i].get("sourceLogo").url();
+                  var feedURL = results[i].get("sourceURL");
+
+                  //start XML parsing
+                  feed(feedURL, function(err, articles) {
+
+                    // loop through the list of articles returned
+                    for (var x = 0; x < articles.length; x++) {
+
+                        returnResults[x] = {};
+                        returnResults[x]["sourceName"] = sourceName
+                        returnResults[x]["fileURL"] = fileURL
+                        returnResults[x]["title"] = articles[x].title;
+                        returnResults[x]["link"] = articles[x].link;
+                        returnResults[x]["content"] = articles[x].content;
+                        returnResults[x]["published"] = articles[x].published;
+
+                        // check we have reached the end of our list of articles & urls
+                        if( x === articles.length-1 && i === results.length-1) {
+                            res.render('hello', {returnResults : returnResults}); // end http response
+                        } // else still have rss urls to check
+                    } //  end 
+                  });
                 }
+                
             },
             error: function(error) {
-              alert("Error: " + error.code + " " + error.message);
+              console.log("Error: " + error.code + " " + error.message);
             }
         });   
+
+
 }
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    retrieveFeedSource();
-    console.log("inside parse method");
-    res.render('hello', {});
+    retrieveFeedSource(res);
+    //console.log("sourcename is" + newResults[1]["sourceName"]);
 });
 
 
